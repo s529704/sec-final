bfd6a05f199f1bd0fb4e81802ca30021fef6e1e9
Dawson McConnell <77599007+s529704@users.noreply.github.com>
2022-11-07 13:12:02 -0600

    Update Main.java

--- src/main/java/pa/pythag/Main.java
+++ src/main/java/pa/pythag/Main.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pa.pythag;
-
+import java.util.Scanner;
 /**
  *
  * @author nathan
@@ -15,7 +15,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scan.nextInt();
+        System.out.println(triple(number));
     }
+    public static boolean triple(int n){
+        int num = (n*n)-((n-1)*(n-1));
+        System.out.println(num);
+        //double check = Math.sqrt(num);
+        //if(check - Math.floor(num) != 0){
+        //    return false;
+        //}
+        //else 
+            return true;
     
 }
+}
+
