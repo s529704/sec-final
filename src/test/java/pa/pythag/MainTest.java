914a9e1a0e6da7ca1e3aedae79cb804851e28be2
Justin Hirner <69913623+AppleSlide@users.noreply.github.com>
2023-11-27 16:05:28 -0600

    More values to test
    Maybe we add a list and loop through it to test?

--- src/test/java/pa/pythag/MainTest.java
+++ src/test/java/pa/pythag/MainTest.java
@@ -10,8 +10,20 @@ public class MainTest {
 
     @Test
     public void testTriple(){
+        //Test if these values are true
         assertTrue(Main.triple(13));
+        assertTrue(Main.triple(25));
+        assertTrue(Main.triple(61));
+        assertTrue(Main.triple(365));
+        assertTrue(Main.triple(101));
+
+
+        //Test if these values are false
         assertFalse(Main.triple(42));
+        assertFalse(Main.triple(18));
+        assertFalse(Main.triple(62));
+        assertFalse(Main.triple(103));
+        assertFalse(Main.triple(294));
     }
 
 }
